c29553fbda0a0205df3746d529009c16dabd61d8
appear <appear.ko@gmail.com>
2024-09-17 20:59:37 +0900

    랜덤하게 정렬하고 카운트에 맞게 짤라냅니다

--- pr/random-assign-reviewer/src/index.ts
+++ pr/random-assign-reviewer/src/index.ts
@@ -4,16 +4,31 @@ import * as github from '@actions/github'
 async function main() {
     const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN')
     const octokit = github.getOctokit(GITHUB_TOKEN)
+    const context = github.context
+    const {pull_request} = context.payload
 
     const ORGANIZATION_NAME = core.getInput('ORGANIZATION_NAME')
-    // const ASSIGN_COUNT = core.getInput('ASSIGN_COUNT')
+    const ASSIGN_COUNT = core.getInput('ASSIGN_COUNT')
 
-    // Organization의 멤버 리스트 가져오기
+    // organization의 멤버 리스트 가져오기
     const members = await octokit.rest.orgs.listMembers({
         org: ORGANIZATION_NAME,
     })
 
-    console.log(members.data) // eslint-disable-line
+    const orgMemberNames = members.data.map(({login}) => login)
+
+    // PR에 리뷰어 어사인
+    await octokit.rest.pulls.requestReviewers({
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        pull_number: pull_request.number,
+        reviewers: shuffledAndSlice(orgMemberNames, Number(ASSIGN_COUNT)),
+    })
+}
+
+function shuffledAndSlice(members: string[], count: number) {
+    const shuffled = members.sort(() => 0.5 - Math.random())
+    return shuffled.slice(0, count)
 }
 
 main()
